c40a80835bfcfb4e186dbec441d694d26685a34a
alexpichler99 <alexander.pichler99@gmx.at>
2016-04-13 09:18:43 +0200

    Merge branch 'master' of https://github.com/alexpichler99/EasyGrow
    Conflicts:
    	JavaFX/.idea/workspace.xml

--- JavaFX/src/main/java/controller/Controller.java
+++ JavaFX/src/main/java/controller/Controller.java
@@ -161,7 +161,6 @@ public class Controller implements Observer {
         }
         catch (Exception e){ }
     }
-    //push-test
     private void storeMainProperties() {
         try {
             FileOutputStream propFile = new FileOutputStream(mainPropertiesFile);
