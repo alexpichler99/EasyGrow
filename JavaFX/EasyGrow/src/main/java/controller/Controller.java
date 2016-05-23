cc578acfb03e0c15a16fa2539e750e30dce61896
alexpichler99 <alexander.pichler99@gmx.at>
2016-05-23 17:49:36 +0200

    .

--- JavaFX/EasyGrow/src/main/java/controller/Controller.java
+++ JavaFX/EasyGrow/src/main/java/controller/Controller.java
@@ -353,7 +353,6 @@ public class Controller implements Observer {
             plantName = prop.getProperty("plantName");
             if (plantName == null)
                 plantName = "";
-            ///fsdf
 
             comboSetHumidityOptimum.setValue((int)humidityOptimum);
             comboSetMoistureOptimum.setValue((int)moistureOptimum);
